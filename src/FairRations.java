cacef30c833a251088f94d8051e38d97433b38d4
Gayrat <gayrat.komilov@gmail.com>
2022-06-14 17:00:29 +0500

    Final release

--- src/FairRations.java
+++ src/FairRations.java
@@ -19,10 +19,31 @@ class FairRationsResult {
      * The function accepts INTEGER_ARRAY B as parameter.
      */
 
-    public static String fairRations(List<Integer> B) {
+    public static String fairRations(List<Integer> b) {
         // Write your code here
+        String returnString = new String();
+        if (b.size()< 3 ) {
+            returnString = "NO";
+            return returnString;
+        }
+        int count = 0;
+        int qoldiq = 0;
+        for (int i = 0; i <b.size() ; i++) {
+            int current = 0;
+            current = b.get(i)  + qoldiq;
 
-        return "T";
+            if (current%2 == 1){
+                qoldiq = 1;
+                count +=2;
+            }
+            else qoldiq = 0;
+        }
+        if (qoldiq == 1){
+            returnString = "NO";
+            return returnString;
+        }
+
+          return String.valueOf(count);
 
     }
 
